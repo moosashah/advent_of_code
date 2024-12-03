380c458944058fcd70b93ee4c5dcdd138f2acb64
Moosa Shah <hello.moosashah@gmail.com>
2024-12-03 10:57:30 +0000

    day 3 part 2

--- 2024/day3/index.spec.ts
+++ 2024/day3/index.spec.ts
@@ -5,6 +5,10 @@ function extractMulMatches(content: string): string[] {
   return content.match(/mul\(\d{1,3},\d{1,3}\)/g) || [];
 }
 
+function removeDontMatches(content: string): string {
+  return content.replace(/don't\(\)(.*?)do\(\)/gs, "");
+}
+
 function multiplyMatches(matches: string[]): number {
   return matches.reduce((acc, match) => {
     const lp = match.indexOf("(");
@@ -38,4 +42,31 @@ test("part 1", async () => {
   const total = multiplyMatches(matches);
 
   expect(total).toBe(166630675);
+});
+
+test("part 2 example", async () => {
+  const content =
+    "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+
+  const cleaned = removeDontMatches(content);
+
+  const matches = extractMulMatches(cleaned);
+
+  const total = multiplyMatches(matches);
+
+  expect(total).toBe(48);
+});
+
+test("part 2", async () => {
+  const content = await readTextFile("./input.txt");
+
+  //catch "don't() until do() and replace with empty string"
+  const cleaned = removeDontMatches(content);
+
+  //then I just do another pass and capture mul groups
+  const matches = extractMulMatches(cleaned);
+
+  const total = multiplyMatches(matches);
+
+  expect(total).toBe(93465710);
 });
