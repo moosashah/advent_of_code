258d4fece43212582eac4d6c000ccc4c6c420bbf
Moosa Shah <hello.moosashah@gmail.com>
2024-12-11 15:10:43 +0000

    refactor

--- 2024/day4/index.spec.ts
+++ 2024/day4/index.spec.ts
@@ -4,57 +4,49 @@ import { readTextFile } from "../utils";
 const XMAS = "XMAS";
 const SAMX = "SAMX";
 
-const right = (row: number, col: number, rows: string[]): string => {
-  if (col + 3 < rows[row].length) {
-    return (
-      rows[row][col] +
-      rows[row][col + 1] +
-      rows[row][col + 2] +
-      rows[row][col + 3]
-    );
-  }
-  return "";
+type Direction = {
+  rowStep: number;
+  colStep: number;
 };
 
-const up = (row: number, col: number, rows: string[]): string => {
-  if (row - 3 >= 0) {
-    return (
-      rows[row][col] +
-      rows[row - 1][col] +
-      rows[row - 2][col] +
-      rows[row - 3][col]
-    );
-  }
-  return "";
-};
+const directions: Direction[] = [
+  { rowStep: -1, colStep: 0 }, //up
+  { rowStep: 0, colStep: 1 }, //right
+  { rowStep: -1, colStep: 1 }, //up-right
+  { rowStep: -1, colStep: -1 }, //up-left
+];
 
-const upRight = (row: number, col: number, rows: string[]): string => {
-  if (row - 3 >= 0 && col + 3 < rows[row].length) {
-    return (
-      rows[row][col] +
-      rows[row - 1][col + 1] +
-      rows[row - 2][col + 2] +
-      rows[row - 3][col + 3]
-    );
-  }
-  return "";
-};
+const check = (
+  row: number,
+  col: number,
+  rowStep: number,
+  colStep: number,
+  input: string[],
+): string => {
+  let match = "";
+  //loop 4 times to get to XMAS or SAMX
+  for (let i = 0; i < 4; i++) {
+    const currRow = row + rowStep * i;
+    const currCol = col + colStep * i;
 
-const upLeft = (row: number, col: number, rows: string[]): string => {
-  if (row - 3 >= 0 && col - 3 >= 0) {
-    return (
-      rows[row][col] +
-      rows[row - 1][col - 1] +
-      rows[row - 2][col - 2] +
-      rows[row - 3][col - 3]
-    );
+    if (
+      //check for out of bounds cells
+      currRow < 0 ||
+      currCol < 0 ||
+      currRow >= input.length ||
+      currCol >= input[currRow].length
+    ) {
+      //early return if out of bounds
+      return match;
+    }
+    match += input[currRow][currCol];
   }
-  return "";
+  return match;
 };
 
 test("part 1 example", () => {
   let count = 0;
-  const rows = `MMMSXXMASM
+  const input = `MMMSXXMASM
 MSAMXMSMSA
 AMXSXMAAMM
 MSAMASMSMX
@@ -66,38 +58,28 @@ MAMMMXMMMM
 MXMXAXMASX`
     .split("\n")
     .filter(Boolean);
-  for (let row = 0; row < rows.length; row++) {
-    for (let col = 0; col < rows[row].length; col++) {
-      up(row, col, rows) === XMAS && count++;
-      right(row, col, rows) === XMAS && count++;
-      upRight(row, col, rows) === XMAS && count++;
-      upLeft(row, col, rows) === XMAS && count++;
 
-      up(row, col, rows) === SAMX && count++;
-      right(row, col, rows) === SAMX && count++;
-      upRight(row, col, rows) === SAMX && count++;
-      upLeft(row, col, rows) === SAMX && count++;
+  for (let row = 0; row < input.length; row++) {
+    for (let col = 0; col < input[row].length; col++) {
+      for (const { rowStep, colStep } of directions) {
+        check(row, col, rowStep, colStep, input) === XMAS && count++;
+        check(row, col, rowStep, colStep, input) === SAMX && count++;
+      }
     }
   }
+
   expect(count).toBe(18);
 });
 
 test("part 1", async () => {
-  const content = (await readTextFile("./input.txt"))
-    .split("\n")
-    .filter(Boolean);
+  const input = (await readTextFile("./input.txt")).split("\n").filter(Boolean);
   let count = 0;
-  for (let row = 0; row < content.length; row++) {
-    for (let col = 0; col < content[row].length; col++) {
-      up(row, col, content) === XMAS && count++;
-      right(row, col, content) === XMAS && count++;
-      upRight(row, col, content) === XMAS && count++;
-      upLeft(row, col, content) === XMAS && count++;
-
-      up(row, col, content) === SAMX && count++;
-      right(row, col, content) === SAMX && count++;
-      upRight(row, col, content) === SAMX && count++;
-      upLeft(row, col, content) === SAMX && count++;
+  for (let row = 0; row < input.length; row++) {
+    for (let col = 0; col < input[row].length; col++) {
+      for (const { rowStep, colStep } of directions) {
+        check(row, col, rowStep, colStep, input) === XMAS && count++;
+        check(row, col, rowStep, colStep, input) === SAMX && count++;
+      }
     }
   }
   expect(count).toBe(2297);
